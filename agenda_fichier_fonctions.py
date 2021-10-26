d593250df3c6c1cc9afcf2b445663b2d8ec7ca71
Tidic <tidic84@gmail.com>
2021-10-26 16:16:22 +0200

    navigation jours

--- agenda_fichier_fonctions.py
+++ agenda_fichier_fonctions.py
@@ -1,3 +1,6 @@
+import microbit
+from time import *
+
 class day():
     """classe créant un objet correspondant
     à une journée : un jour, ce que l'on prévoit de faire/où/quelle h"""
@@ -45,7 +48,7 @@ def modifier_event(date, modif, new_data):
     with open(date+".event", "wb") as nv_fichier:
         nv_fichier.write(":".join(list_1))
         
-       
+
 def supp_event(date, elemsupp):
     # quel événement du jour *date* je veux modifier
     list_1 = get_events(date)
@@ -53,3 +56,36 @@ def supp_event(date, elemsupp):
     #j'écris le nv contenu
     with open(date+".event", "wb") as nv_fichier:
         nv_fichier.write(":".join(list_1))
+
+#add_event("27-10-2021","1/2/3/4")
+#add_event("27-10-2021","9/0/4/8")
+print(get_events("27-10-2021"))
+
+tm_mday = localtime().tm_mday
+tm_mon = localtime().tm_mon
+tm_year = localtime().tm_year
+
+def show_events(day, month, year):
+    date = str(day)+"-"+str(month)+"-"+str(year)
+    print(date)
+    try:
+      get_events(date)
+    except:
+      print("Il n'y a pas d'évenemnents ce jour ci")
+    else:
+      print("Aujourd'hui,", get_events(date))
+
+
+show_events(tm_mday, tm_mon, tm_year)
+
+while True:
+    if microbit.button_a.was_pressed():
+        tm_mday -= 1
+        show_events(tm_mday, tm_mon, tm_year)
+    if microbit.button_b.was_pressed():
+        tm_mday += 1
+        show_events(tm_mday, tm_mon, tm_year)
+    
+    
+    
+
