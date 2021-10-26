b3e5f2ad8649565c25ea8e8fc6e2855b04fc9549
Tidic <tidic84@gmail.com>
2021-10-26 12:03:34 +0200

    add events

--- agenda_fichier_fonctions.py
+++ agenda_fichier_fonctions.py
@@ -1,3 +1,4 @@
+
 class day():
     """classe créant un objet correspondant
     à une journée : un jour, ce que l'on prévoit de faire/où/quelle h"""
@@ -7,23 +8,37 @@ class day():
         self.event = activity[0]
         self.place = activity[1]
         self.time = activity[2]
+        self.period = activity[3]
 
 
 def add_event(date, event):
+  try:
+    get_event(date)
+  except:
     event = event.split("/")
     jour = day(date, event)
-    with open("{}".format("/events/"+date+".event"), "wb") as day_n:
+    with open("{}".format(date+".event"), "wb") as day_n:
         day_n.write(jour.date + "/")
         day_n.write(jour.event + "/")
         day_n.write(jour.place + "/")
-        day_n.write(jour.time)
+        day_n.write(jour.time + "/")
+        day_n.write(jour.period)
+  else:
+    event = event.split("/")
+    jour = day(date, event)
+    with open("{}".format(date+".event"), "ab") as day_n:
+        day_n.write(":" + jour.date + "/")
+        day_n.write(jour.event + "/")
+        day_n.write(jour.place + "/")
+        day_n.write(jour.time + "/")
+        day_n.write(jour.period)
         
 
 def get_event(date):
-    with open("{}".format("/events/"+date+".event"), "rb") as info_n:
+    with open("{}".format(date+".event"), "rb") as info_n:
         paper = info_n.read()
     return paper
 
-add_event("26-10-2021", "rien/nulle part/16:16")
+add_event("26-10-2021", "rien/nulle part/16:16/3h")
 event = get_event("26-10-2021").split("/")
 print(event)
